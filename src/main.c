4533f1a079627fe76fe62c8a86c47dc437f23baa
Script <bigalex934@gmail.com>
2024-01-31 15:55:21 +0300

    Rollback async related changes

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <stdint.h>
-#include <stdio.h>
 
 #include "core/common.h"
 #include "core/opt_parser.h"
@@ -9,9 +8,11 @@ int32_t main(int32_t argc, char *argv[]) {
     struct Options *opts = parse_opts(argc, argv);
     struct HttpServer *server = create_server("", opts->port);
     init_server(server);
-    run(server);
 
-    // release_server(server);
+    while (ALWAYS) {
+        handle_pending_request(server);
+    }
+    release_server(server);
 
     return 0;
 }
