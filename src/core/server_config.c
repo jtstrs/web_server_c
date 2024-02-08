622b829c27562d707903c2a24b3c52f67541add6
Script <bigalex934@gmail.com>
2024-02-08 17:39:37 +0300

    Fix errors in config parser

--- src/core/server_config.c
+++ src/core/server_config.c
@@ -20,10 +20,13 @@ void parse_line(ServerConfig *conf, char *line) {
     static const char *delims = "=";
     char *key_token = strtok(line, delims);
 
+
     if (!key_token) {
         log_message(ERROR_LEVEL, "Couldnt parse config token");
         return;
     }
+
+    log_message(DEBUG_LEVEL, "Key config token: %s", key_token);
 
     char *value_token = strtok(NULL, delims);
 
@@ -32,12 +35,14 @@ void parse_line(ServerConfig *conf, char *line) {
         return;
     }
 
-    if (strcmp(key_token, "host")) {
+    log_message(DEBUG_LEVEL, "Value token: %s", value_token);
+
+    if (strcmp(key_token, "host") == 0) {
         strncpy(conf->host, value_token, HOST_NAME_LENGTH);
-    } else if (strcmp(key_token, "port")) {
-        int32_t parsed_val = parse_int32(key_token);
+    } else if (strcmp(key_token, "port") == 0) {
+        int32_t parsed_val = parse_int32(value_token);
         conf->port = (parsed_val == -1) ? DEFAULT_PORT_VALUE : parsed_val;
-    } else if (strcmp(key_token, "storage_path")) {
+    } else if (strcmp(key_token, "storage_path") == 0) {
         strncpy(conf->storage_path, value_token, STORAGE_PATH_SIZE);
     } else {
         log_message(DEBUG_LEVEL, "Unknown configuration option %s", key_token);
@@ -59,7 +64,6 @@ ServerConfig *parse_config(char *config_path) {
     memset(line, 0, MAX_CONFIG_LINE_SIZE);
 
     while (!ferror(conf) && !feof(conf) && fgets(line, MAX_CONFIG_LINE_SIZE, conf) != NULL) {
-        log_message(DEBUG_LEVEL, "Parse config line: %s", line);
         parse_line(&server_conf, line);
         memset(line, 0, MAX_CONFIG_LINE_SIZE);
     }
