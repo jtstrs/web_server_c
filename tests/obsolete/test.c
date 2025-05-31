a25cd45e072b11d39691808a2bff6b758bae1f0d
Script <bigalex934@gmail.com>
2025-05-31 21:44:35 +0300

    Remove unecessary stuff

--- tests/obsolete/test.c
+++
@@ -1,7 +0,0 @@
-#include "client.h"
-
-int32_t main(int32_t argc, char *argv[]) {
-    struct Client *client = create_client("localhost", 12000);
-    ping_request(client);
-    release_client(client);
-}
